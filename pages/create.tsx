c9cd27782f5bc7aed448517a4c6280a7b94ebe2d
yanzayd <nelsonnyzbedel@gmail.com>
2022-10-30 11:08:54 +0200

    the update of create file

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import {
   useAddress,
@@ -11,6 +11,7 @@ import {
   useCreateAuctionListing,
 } from "@thirdweb-dev/react";
 import { Contract } from "ethers";
+import { NFT } from "@thirdweb-dev/sdk";
 
 type Props = {};
 
@@ -22,12 +23,16 @@ function Create({}: Props) {
     "marketplace"
   );
 
+  const [selectedNft, setSelectedNft] = useState<NFT>();
+
   const { contract: collectionContract } = useContract(
     process.env.NEXT_PUBLIC_COLLECTION_CONTRACT,
     "nft-collection"
   );
 
   const ownedNfts = useOwnedNFTs(collectionContract, address);
+
+  // console.log(selectedNft);
 
   //   console.log("the address is:  ", address);
   //   console.log("the owened Nfts: ", ownedNfts);
@@ -43,9 +48,71 @@ function Create({}: Props) {
 
         <hr className="mb-5" />
 
-        <p>Below you will find the NFT's you own in ypur wallet</p>
+        <p>Below you will find the NFT's you own in your wallet</p>
 
-        <div></div>
+        <div className="flex overflow-x-scroll space-x-2 p-4">
+          {ownedNfts?.data?.map((nft) => (
+            <div
+              key={nft.metadata.id}
+              onClick={() => setSelectedNft(nft)}
+              className={`flex flex-col space-y-2 card min-w-fit border-2 bg-gray-100 ${
+                nft.metadata.id === selectedNft?.metadata.id
+                  ? "border-black"
+                  : "border-transparent"
+              }`}
+            >
+              <MediaRenderer
+                className="h-48 rounded-lg"
+                src={nft.metadata.image}
+              />
+
+              <p className="text-lg truncate font-bold">{nft.metadata.name}</p>
+              <p className="text-xs truncate ">{nft.metadata.description}</p>
+            </div>
+          ))}
+        </div>
+
+        {selectedNft && (
+          <form className="">
+            <div className="flex flex-col p-10">
+              <div className="grid grid-col-2 gap-5">
+                <label className="border-r font-light">
+                  Direct Listing / Fixed Price
+                </label>
+                <input
+                  type="radio"
+                  name="listingType"
+                  value="directListing"
+                  className="ml-auto h-10 w=10"
+                />
+
+                <label className="border-r font-light">Auction</label>
+                <input
+                  type="radio"
+                  name="listingType"
+                  value="auctionListing"
+                  className="ml-auto h-10 w=10"
+                />
+
+                <label border-r font-light>
+                  Price
+                </label>
+                <input
+                  type="text"
+                  name="price"
+                  placeholder="0.05"
+                  className="bg-gray-100 p-5"
+                />
+              </div>
+              <button
+                className="bg-blue-600 text-white rounded-lf p-4 mt-8"
+                type="aubmit"
+              >
+                Create Listing
+              </button>
+            </div>
+          </form>
+        )}
       </main>
     </div>
   );
